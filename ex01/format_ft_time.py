2ff0e0b85c39579fbc4220085dadd30bd71340b2
= <kuba.kaczmarski312@gmail.com>
2023-05-02 16:51:57 +0200

    ex01

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,3 +1,10 @@
+import time
 import datetime
+import calendar
 
-now = datetime.date.today()
+date = datetime.date.today()
+now = time.time()
+
+print("Seconds since January 1, 1970:",
+      f"{now:,.4f}", " or ", f"{now:.2e}", " in scientific notation")
+print(f"{calendar.month_name[10] :.3}", date.day, date.year)
